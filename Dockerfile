30c97d2230d1c336616d61b64df8821d591c8e47
Sergio <serrojjim@alum.us.es>
2022-07-12 16:17:15 +0200

    modificaciones bat start

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,5 @@
 FROM python:3.8-slim-buster
 WORKDIR /
-COPY disconnecter.py .
-COPY disconnecter.config .
-COPY docker-compose.override.yml .
 COPY requirements.txt .
 RUN pip install -r requirements.txt
-ENTRYPOINT python ./disconnecter.py 
+CMD cd src && python disconnecter.py
--- starter.bat
+++ starter.bat
@@ -1,11 +1,13 @@
 @ECHO OFF 
 ECHO  --------------- Starting Disconnect --------------- 
 ECHO  [BATCH PROCESS INFO] Cargando configuracion
-START /B python disconnecter.py
+START /B /WAIT docker build -t configurador-imagen .
+START /B /WAIT docker run --rm -d --name configurador -v "%cd%:/src" configurador-imagen
+
 ECHO  [BATCH PROCESS INFO] Contenedores buildeados
-START /B /WAIT docker-compose build
 ECHO  [BATCH PROCESS INFO] Desplegando contenedores
-START /B /WAIT docker-compose up -d ApiGateWay
+START /B /WAIT docker-compose up --build -d ApiGateWay
 ECHO  [BATCH PROCESS INFO] Proceso finalizado (Checkea los contenedores levantados)
+@pause
 
 
