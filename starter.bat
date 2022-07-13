3ee8d66594cc179a7bb956445c4deedc315ccec3
Sergio <serrojjim@alum.us.es>
2022-07-13 14:52:42 +0200

    Modificado .bat

--- starter.bat
+++ starter.bat
@@ -4,7 +4,7 @@ ECHO  [BATCH PROCESS INFO] Cargando configuracion
 START /B /WAIT docker build -t configurador-imagen .
 START /B /WAIT docker run --rm -d --name configurador -v "%cd%:/src" configurador-imagen
 ECHO  [BATCH PROCESS INFO] Copiando el archivo de configuración al ApiGateWay
-START /B xcopy disconnecter.config apigateway\src\main\resources
+START /B xcopy disconnecter.config apigateway\src\main\resources /Y
 
 ECHO  [BATCH PROCESS INFO] Contenedores buildeados
 ECHO  [BATCH PROCESS INFO] Desplegando contenedores
