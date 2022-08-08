700dd3dbe3d7e4953ed0594b4fca4fd327475419
xNegis <xnegis@gmail.com>
2022-08-08 19:12:27 +0200

    Docker connected

--- starter.bat
+++ starter.bat
@@ -3,12 +3,12 @@ ECHO  --------------- Starting Disconnect ---------------
 ECHO  [BATCH PROCESS INFO] Cargando configuracion
 START /B /WAIT docker build -t configurador-imagen .
 START /B /WAIT docker run --rm -d --name configurador -v "%cd%:/src" configurador-imagen
-ECHO  [BATCH PROCESS INFO] Copiando el archivo de configuración al ApiGateWay
+ECHO  [BATCH PROCESS INFO] Copiando el archivo de configuración al apigateway
 START /B xcopy disconnecter.config apigateway\src\main\resources /Y
 
 ECHO  [BATCH PROCESS INFO] Contenedores buildeados
 ECHO  [BATCH PROCESS INFO] Desplegando contenedores
-START /B /WAIT docker-compose up --build -d ApiGateWay
+START /B /WAIT docker-compose up --build -d apigateway
 ECHO  [BATCH PROCESS INFO] Proceso finalizado (Checkea los contenedores levantados)
 @pause
 
